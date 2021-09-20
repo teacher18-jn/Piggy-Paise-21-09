72f1ee06257468e98d121501c48b43241e171a57
teacher18-jn <84465689+teacher18-jn@users.noreply.github.com>
2021-09-21 00:24:45 +0530

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,23 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";;
+import BottomTabNavigator from "./BottomTabNavigator";
+import Profile from "../screens/Profile";
+import Logout from "../screens/Logout";
+import Goals from "../screens/Goals";
+
+const Drawer = createDrawerNavigator();
+
+export default class DrawerNavigator extends React.Component{
+  render(){
+    return(
+        <Drawer.Navigator>
+            <Drawer.Screen name = "Home" component ={BottomTabNavigator}/>
+            <Drawer.Screen name = "Profile" component = {Profile}/>
+            <Drawer.Screen name = "Goals" component = {Goals}/>
+            <Drawer.Screen name = "Logout" component = {Logout}/>
+        </Drawer.Navigator>
+    )
+  }
+}
+
